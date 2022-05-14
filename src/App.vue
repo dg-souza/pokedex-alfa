61875cf55900929aef75acc09b55b564f1558508
dg-souza <davisouzag@gmail.com>
2022-05-14 20:45:32 -0300

    :sparkles: NewFeature(App): Utilização da api para pegar infos dos pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <main>
+      <div class="search">
+        <input
+          class="search-input"
+          type="search"
+          placeholder="Search"
+          v-model="search"
+        />
+        <span class="search-focus"></span>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Api from './services/api'
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      search: '',
+      pokemons: []
+    }
+  },
   components: {
-    HelloWorld
+    
+  },
+  async created() {
+    const { data : { results } } = await Api.getAll()
+    
+    for(const result of results ) {
+      const { data } = await Api.getPokemon(result.name)
+      const obj = {
+        id: data.id,
+        name: data.name,
+        image: data.sprites.front_default,
+        types: data.types,
+        experience: data.base_experience,
+        stats: [data.stats[0], data.stats[1], data.stats[2]]
+      }
+      this.pokemons.push(obj)
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #ededed;
+}
+
+.container {
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+
+  main {
+    width: 100%;
+
+    .search {
+      display: flex;
+      flex-direction: column;
+      padding: 7px;
+
+      &-input {
+        height: 35px;
+        font-size: 15px;
+        border: none;
+        outline: none;
+        background-color: transparent;
+        color: rgb(124, 123, 123);
+      }
+
+      &-focus {
+        width: 100%;
+        height: 2px;
+        background-color: #ccc;
+        transition: 0.4s;
+      }
+
+      &:focus &-focus {
+        background-color: red;
+        transition: 0.4s;
+      }
+    }
+  }
 }
 </style>
